ecc5b5ad6b5481a91fc704eb965a5415898c0e7a
shabil15 <75298402+shabil15@users.noreply.github.com>
2023-08-17 17:39:49 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+
+const toTop = document.getElementById("top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
